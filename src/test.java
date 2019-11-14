cfa65490a2e59948c234d6c90d6c6f719c186ccc
shenghaishxt <shenghaishxt@gmail.com>
2019-11-14 17:42:56 +0800

    014

---
+++ src/test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+        String x = "qwe";
+        String i = "qw";
+        String[] strs = {x, i};
+        String aa = x.substring(0, 1);
+        System.out.println(aa);
+    }
+}
