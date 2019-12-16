e8ca3acd74533ee97bd419deece056f2c04cb6d9
shenghaishxt <shenghaishxt@gmail.com>
2019-12-16 14:11:29 +0800

    20191216

--- src/com/shenghaishxt/easy/_0101.java
+++ src/com/shenghaishxt/easy/_0101.java
@@ -7,20 +7,23 @@ import java.util.Queue;
 
 public class _0101 {
     private boolean isSymmetric1(TreeNode root) {
+        // 和题_0100十分类似。
         // 直接造个新的树与自己比较！
         // 什么时候为对称二叉树？(1)根节点有相同的值。(2)每个树的右子树与另一个树的左子树镜像对称。
         return helper(root, root);
     }
 
     private boolean helper(TreeNode t1, TreeNode t2) {
-//      1.找终止条件(两个二叉树都为空时返回True，只有一个为空时返回False)
+        // 1.找终止条件(两个二叉树都为空时返回True，只有一个为空时返回False)
         if (t1 == null && t2 == null)
             return true;
         if (t1 == null || t2 == null)
             return false;
-//      2.找返回值(返回子树是否为对称二叉树)
-//      3.本级递归应该做什么(1.判断根节点的值是否相等 2.判断A的右子树和B的左子树是否对称 3.判断A的左子树和B的右子树是否对称)
-        return t1.val == t2.val && helper(t1.left, t2.right) && helper(t1.right, t2.left);
+        // 2.找返回值(A的右子树和B的左子树是否对称，以及A的左子树和B的右子树是否对称)
+        boolean left = helper(t1.left, t2.right);
+        boolean right = helper(t1.right, t2.left);
+        // 3.本级递归应该做什么
+        return left && right && t1.val == t2.val;
     }
 
     private boolean isSymmetric2(TreeNode root) {
