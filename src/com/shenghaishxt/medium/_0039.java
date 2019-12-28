7e1379fe321bc6fd8f1cf1333f7dff16ca9279d2
shenghaishxt <shenghaishxt@gmail.com>
2019-12-28 22:19:31 +0800

    20191228

--- src/com/shenghaishxt/medium/_0039.java
+++ src/com/shenghaishxt/medium/_0039.java
@@ -16,45 +16,33 @@ import java.util.*;
  *         撤销选择
  */
 public class _0039 {
-    private Set<List<Integer>> res = new HashSet<>();
+    private List<List<Integer>> res = new ArrayList<>();
 
     private List<List<Integer>> combinationSum(int[] candidates, int target) {
         if (candidates.length == 0)
             return new ArrayList<>();
-        ArrayList<Integer> cur = new ArrayList<>();
-
-        backTrack(cur, candidates, target);
-        return new ArrayList<>(res);
+        Arrays.sort(candidates);
+        backTrack(new Stack<>(), candidates, target, 0);
+        return res;
     }
 
-    private void backTrack(ArrayList<Integer> cur, int[] candidates, int target) {
-        if (getSum(cur) == target) {
-            List<Integer> newCur = new ArrayList<>(cur);
-            Collections.sort(newCur);
-            res.add(new ArrayList<>(newCur));
+    private void backTrack(Stack<Integer> cur, int[] candidates, int residue, int begin) {
+        if (residue == 0) {
+            res.add(new ArrayList<>(cur));
             return;
         }
 
-        for (int candidate : candidates) {
-            if (getSum(cur) > target)
-                return;
-            cur.add(candidate);
-            backTrack(cur, candidates, target);
-            cur.remove(cur.size()-1);
+        for (int i = begin; i < candidates.length && residue-candidates[i] >= 0; i++) {
+            cur.push(candidates[i]);
+            backTrack(cur, candidates, residue-candidates[i], i);
+            cur.pop();
         }
-    }
-
-    private int getSum(ArrayList<Integer> cur) {
-        int sum = 0;
-        for (int i : cur)
-            sum += i;
-        return sum;
     }
 
     public static void main(String[] args) {
         _0039 Sol = new _0039();
-        int[] candidates = {1,2};
-        int target = 4;
+        int[] candidates = {2,3,6,7};
+        int target = 7;
         List<List<Integer>> res = Sol.combinationSum(candidates, target);
         System.out.println(res);
     }
