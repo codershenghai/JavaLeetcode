b893ef1ad2d8e69685344b7dd6d137374b916896
shenghaishxt <shenghaishxt@gmail.com>
2020-01-07 17:03:02 +0800

    20200107

---
+++ src/com/shenghaishxt/medium/_0082.java
@@ -0,0 +1,38 @@
+package com.shenghaishxt.medium;
+
+import com.shenghaishxt.structure.ListNode;
+
+public class _0082 {
+    private ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode pre = dummy;
+        while (head != null && head.next != null) {
+            if (head.val != head.next.val) {
+                pre = head;
+                head = head.next;
+            } else {
+                while (head.next != null && head.val == head.next.val)
+                    head = head.next;
+                pre.next = head.next;
+                head = head.next;
+            }
+        }
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        _0082 Sol = new _0082();
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(1);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(2);
+        head.next.next.next.next = new ListNode(3);
+        ListNode res = Sol.deleteDuplicates(head);
+
+        while (res != null) {
+            System.out.println(res.val);
+            res = res.next;
+        }
+    }
+}
