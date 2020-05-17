6a850f464c18328435f8d64861e22ae43c2dd698
shenghaishxt <shenghaishxt@gmail.com>
2020-05-17 13:27:36 +0800

    20200517

---
+++ src/com/shenghaishxt/medium/_0131.java
@@ -0,0 +1,15 @@
+package com.shenghaishxt.medium;
+
+import java.util.List;
+
+public class _0131 {
+//    public List<List<String>> partition(String s) {
+//    }
+//
+//    private boolean checkPalindrome(String str, int left, int right) {
+//        while (left < right) {
+//            if (str.charAt(left++) != str.charAt(right--)) return false;
+//        }
+//        return true;
+//    }
+}
