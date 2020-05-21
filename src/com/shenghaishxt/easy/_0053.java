19d556ae4640175fccb851da258be457c9c38a5c
shenghaishxt <shenghaishxt@gmail.com>
2020-05-21 16:40:02 +0800

    20200520

--- src/com/shenghaishxt/easy/_0053.java
+++ src/com/shenghaishxt/easy/_0053.java
@@ -4,19 +4,20 @@ import java.util.Arrays;
 
 public class _0053 {
     // 动态规划
-    private int maxSubArray1(int[] nums) {
-        int[] f = new int[nums.length];
-        f[0] = nums[0];
-        int max_val = nums[0];
+    public int maxSubArray(int[] nums) {
+        if (nums.length == 0) return 0;
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
 
-        // f[i] = max(f[i-1]+nums[i], nums[i])
-        for (int i=1; i<nums.length; i++) {
-            f[i] = Math.max(f[i-1]+nums[i], nums[i]);
-            if (f[i] > max_val)
-                max_val = f[i];
+        // dp[i]定义为以nums[i]为结尾的最大子数组和
+        // dp[i]有两种选择，要么与前面的相邻子数组连接，形成一个和更大的子数组；要么自成一派，自己成为一个子数组。
+        // dp[i] = max(nums[i], nums[i]+dp[i-1])
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = Math.max(nums[i], nums[i] + dp[i-1]);
         }
-        return max_val;
+        return Arrays.stream(dp).max().getAsInt();
     }
+
 
     // 贪心
     private int maxSubArray2(int[] nums) {
@@ -33,9 +34,9 @@ public class _0053 {
 
     public static void main(String[] args) {
         _0053 Sol = new _0053();
-        System.out.println(Sol.maxSubArray2(new int[] {1}));  // 1
-        System.out.println(Sol.maxSubArray2(new int[] {-1}));  // -1
-        System.out.println(Sol.maxSubArray2(new int[] {-2,1}));  // 1
-        System.out.println(Sol.maxSubArray2(new int[] {-2,1,-3,4,-1,2,1,-5,4}));  // 6
+        System.out.println(Sol.maxSubArray(new int[] {1}));  // 1
+        System.out.println(Sol.maxSubArray(new int[] {-1}));  // -1
+        System.out.println(Sol.maxSubArray(new int[] {-2,1}));  // 1
+        System.out.println(Sol.maxSubArray(new int[] {-2,1,-3,4,-1,2,1,-5,4}));  // 6
     }
 }
